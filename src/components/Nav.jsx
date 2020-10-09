2de7f51d4999ffbbac5325eddf622df5e6fe21a0
DaLVeRS2001 <vladbiricov@mail.ru>
2020-10-09 16:38:13 +0300

    3

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,19 +3,19 @@ import React from "react";
 const Nav = () => {
 	return (
 		<nav>
-			<div className="nav-items">
+			<div className="items">
 				<a href="">Profile</a>
 			</div>
-			<div className="nav-items">
+			<div className="items">
 				<a href="">Messages</a>
 			</div>
-			<div className="nav-items">
+			<div className="items">
 				<a href="">News</a>
 			</div>
-			<div className="nav-items">
+			<div className="items">
 				<a href="">Music</a>
 			</div>
-			<div className="nav-items">
+			<div className="items">
 				<a href="">Settings</a>
 			</div>
 		</nav>
